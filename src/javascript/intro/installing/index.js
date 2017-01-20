f8fcb6b85f0790d63c5e274e9e08a0e320038ff9
Patrick Logan <jpatricklogan@gmail.com>
2017-01-20 12:51:59 -0700

    add npm install

--- src/javascript/intro/installing/index.js
+++ src/javascript/intro/installing/index.js
@@ -3,6 +3,6 @@ export const name = 'Patrick';
 export const email = 'jpatricklogan@gmail.com';
 
 // Write code here
-console.log('Patrick');
-console.log('(801)913-3733');
-console.log('jpatricklogan@gmail.com');
+console.log ('Patrick');
+console.log ('(801)913-3733');
+console.log ('jpatricklogan@gmail.com');
