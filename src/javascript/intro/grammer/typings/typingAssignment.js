b18bf9d3a1636b697fea601a8b9bb82d76cb1bba
CaptRat <jpatricklogan@gmail.com>
2017-01-24 12:09:04 -0700

    Typing assignment

---
+++ src/javascript/intro/grammer/typings/typingAssignment.js
@@ -0,0 +1,19 @@
+age = 35;
+string = "hello world";
+array = ["this", "is", "an", "array"];
+let x = 1;
+infinity = 41/0;
+func = function(){ 4 + 3}
+let foo = null;
+
+
+console.log(typeof(age));
+console.log(typeof(string));
+console.log(typeof(array));
+console.log(typeof(x));
+console.log(foo);
+// console.log(typeof(infinity));
+
+console.log(typeof(foo));
+console.log(typeof(func));
+// console.log(typeof(Symbol());
