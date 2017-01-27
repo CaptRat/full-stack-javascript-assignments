bc8e76cc628f0ab874cdbc2eb73f4ffef314a4b9
Patrick Logan <jpatricklogan@gmail.com>
2017-01-27 10:13:28 -0700

    strings homework

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -2,8 +2,8 @@ export const greeting = 'Hello World';
 
 export const characterAt = greeting.charAt(4);
 
-export const greetingArray = greeting.split;
+export const greetingArray = greeting.split('');
 
-export const begins(str, char) => str[0];
+export const begins = (str, char) => {return greeting.startsWith(char)};
 
-export const beginsInsensitive = (str, char) => str[0].toLowerCase();
+export const beginsInsensitive = (str, char) => str[0].toLowerCase() === char.toLowerCase();
