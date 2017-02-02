5b1afc18c83afc26c914127f643352debbf1b65c
CaptRat <jpatricklogan@gmail.com>
2017-02-01 19:20:47 -0700

    scope homework

--- src/javascript/intro/grammer/scope-hoisting-closures/index.js
+++ src/javascript/intro/grammer/scope-hoisting-closures/index.js
@@ -1,20 +1,22 @@
 export const ssn = value => {
 
-  const sanitize = () => value.replace(/\D+/g, '');
+  const sanitize = () => value.replace(/\D+/g, ''); // this RegEx removes non-digit characters such as letters, spaces, punctuation
 
   const format = (value) => `${value.substring(0, 3)}-${value.substring(3, 5)}-${value.substring(5)}`;
+  const sanitizedInput = sanitize();
 
   const isValid = () => {
-    // Your code goes here....
-    //  return ??
+
+    return sanitizedInput.length === 9;
   };
 
   return {
 
     isValid,
+    sanitize,
 
     toString(){
-
+      return format(sanitizedInput)
     }
   };
 };
