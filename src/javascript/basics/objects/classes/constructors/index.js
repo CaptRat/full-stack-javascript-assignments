d1c44e175d0ecb9e37ea068064f2c90f8ddde82f
CaptRat <jpatricklogan@gmail.com>
2017-02-02 23:06:13 -0700

    unfinished homework

--- src/javascript/basics/objects/classes/constructors/index.js
+++ src/javascript/basics/objects/classes/constructors/index.js
@@ -1,1 +1,26 @@
 // Code goes here...
+
+ export class Vehicle{
+   constructor(engineType, numberOfDoors, make) {
+     this.engineType = engineType;
+     this.numberOfDoors = numberOfDoors;
+     this.make = make;
+   }
+ }
+
+ export class Truck extends Vehicle{
+   constructor(engineType, numberOfDoors, make) {
+     super(engineType = 'V8', numberOfDoors = 1, make)
+   };
+ }
+
+  export class Car extends Vehicle{
+    constructor (engineType = 'V4', numberOfDoors = 4, make) {
+      super(engineType, numberOfDoors, make)
+    }
+  }
+
+  export const Prius = new Car(V4, 4, Prius);
+
+  export const F150 = new Truck(V8, 2, F150);
+ }
